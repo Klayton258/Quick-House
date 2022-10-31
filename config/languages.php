a207728f9e6f9f1385bc705d1ee88a1cf16cb81c
edsontomas54 <donthimas54@gmail.com>
2022-10-31 12:00:08 +0200

    Switch Language Implemetation v1

---
+++ config/languages.php
@@ -0,0 +1,15 @@
+<?php
+
+
+return [
+    'en' => [
+        'display' => 'English',
+        'flag-icon' => 'us'
+    ],
+   
+    'pt' => [
+        'display' => 'Portuguese',
+        'flag-icon' => 'pt'
+    ],
+];
+
---
+++ resources/lang/en/message.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'welcome'=>"FIND YOUR HOUSE",
+    'slogan'=> "The right place for you and your family",
+    'contact'=> "Contact",
+    'text_besthome' => "Best Home For You",
+    'home' =>           "Home",
+    'subscribe'=>        "Subscribe",
+    'houses' =>           "Houses",
+];
